14de60442304234da64d2366f3d04ab73ddab4d3
Bùi Đức Thiện <23521475@gm.uit.edu.vn>
2024-10-17 09:08:39 -0700

    Update Dashboard.cs

--- LoginForm/Dashboard.cs
+++ LoginForm/Dashboard.cs
@@ -13,12 +13,23 @@ namespace LoginForm
     public partial class Dashboard : Form
     {
         private string username;
+        private int userId; // Thêm ID người dùng
 
         public Dashboard(string username)
         {
             InitializeComponent();
             this.username = username;
             lblName.Text = username;
+
+        }
+
+        // Hàm khởi tạo với ID người dùng
+        public Dashboard(string username, int userId)
+        {
+            InitializeComponent();
+            this.username = username;
+            lblName.Text = username;
+            this.userId = userId; // Gán ID người dùng
         }
         
         private void btnClose_Click(object sender, EventArgs e)
