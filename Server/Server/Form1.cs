51cdeda6a18e9ebf1fe40629ea64516e82e5eff2
Bùi Đức Thiện <23521475@gm.uit.edu.vn>
2024-10-17 23:01:40 +0700

    Add files via upload
    Server chưa chỉnh sửa UI

---
+++ Server/Server/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener server;
+        private List<TcpClient> clients = new List<TcpClient>();
+        private bool isRunning = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            isRunning = true;
+            Thread acceptThread = new Thread(AcceptClients);
+            acceptThread.Start();
+            MessageBox.Show("Server started...");
+        }
+
+        private void AcceptClients()
+        {
+            while (isRunning)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                clients.Add(client);
+                Thread clientThread = new Thread(HandleClient);
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                try
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead == 0) break;
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Invoke(new Action(() => lstChat.Items.Add("Client " + message))); // Cập nhật ListBox từ luồng khác
+                    BroadcastMessage("Client " + message);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            clients.Remove(client);
+            client.Close();
+        }
+
+        private void BroadcastMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            foreach (var client in clients)
+            {
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMessage.Text)) return;
+
+            string message = "Server: " + txtMessage.Text;
+            lstChat.Items.Add(message);
+            BroadcastMessage(message);
+            txtMessage.Clear();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            isRunning = false;
+            server.Stop();
+            base.OnFormClosing(e);
+        }
+    }
+}
