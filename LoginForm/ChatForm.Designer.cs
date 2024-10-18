c9be1533f373e35d6f3ece553f8b41c828c7ef30
Bùi Đức Thiện <23521475@gm.uit.edu.vn>
2024-10-17 23:22:44 -0700

    Email-Client đã thêm TCP

---
+++ LoginForm/ChatForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace LoginForm
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 98);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(244, 70);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(279, 12);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.ReadOnly = true;
+            this.txtChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtChat.Size = new System.Drawing.Size(509, 426);
+            this.txtChat.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 202);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(197, 66);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "ChatForm";
+            this.Text = "ChatForm";
+            this.Load += new System.EventHandler(this.ChatForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
---
+++ LoginForm/ChatForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class ChatForm : Form
+    {
+        private int userId; // ID người dùng
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public ChatForm(int userId)
+        {
+            InitializeComponent();
+            this.userId = userId;
+            StartClient();
+        }
+
+        public ChatForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void StartClient()
+        {
+            client = new TcpClient("127.0.0.1", 5000);
+            stream = client.GetStream();
+            // Mở một thread hoặc Task để nhận dữ liệu từ server
+            Task.Run(() => ReceiveMessages());
+        }
+        private void ChatForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text;
+            if (!string.IsNullOrEmpty(message))
+            {
+                string formattedMessage = $"{userId}: {message}";
+                byte[] data = Encoding.UTF8.GetBytes(formattedMessage);
+                stream.Write(data, 0, data.Length);
+                txtMessage.Clear();
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead > 0)
+                {
+                    string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Invoke((MethodInvoker)delegate
+                    {
+                        txtChat.AppendText(receivedMessage + Environment.NewLine);
+                    });
+                }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            stream?.Close();
+            client?.Close();
+        }
+    }
+}
